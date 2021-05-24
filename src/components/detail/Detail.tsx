ae58c812b1c27976453137e26456d497d1dfd9df
Scott <scottdrichards@hotmail.com>
2021-05-24 13:20:03 -0400

    Comments and legibility enhancements

--- src/components/detail/Detail.tsx
+++ src/components/detail/Detail.tsx
@@ -1,79 +1,91 @@
 import React, { useEffect, useState } from "react";
-import { useHistory } from "react-router";
 import { getPokemon, PokemonAttributes } from "../../API";
+import { ColorString, DominantColors, luminance } from "../../utils/color";
 import { CapitalFirst } from "../../utils/utils";
-import { ColorString, DominantColors } from "../../utils/color"
-import styles from './Detail.module.css';
-import { PokeType } from "../pokeType/PokeType";
 import { Meter } from "../meter/Meter";
-
+import { PokeType } from "../pokeType/PokeType";
+import styles from './Detail.module.css';
 
 interface DetailProps{id:number}
+
 const Detail = ({id}:DetailProps)=>{
+
+    
     ////////////////
     // Set up hooks
-
+    
     // State management
     const [attrs, setAttrs] = useState<PokemonAttributes&{styleVars?:React.CSSProperties}|undefined>(undefined);
     
-    // Used for the 'go back' button
-    const history = useHistory();
-    
-    // Download data when id (not state) changes
+    // Download data
     useEffect(()=>{
         getPokemon(id).then(res=>res.data).then(attrs=>{
-            // Grab the image to find out color scheme
-            DominantColors(attrs.image/*url*/).then(colors=>{
+            // Figure out the color scheme
+            // Hack to get around CORS https://lokeshdhakar.com/projects/color-thief/
+            const googleProxyURL = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=';
+            const urls = [attrs.image,googleProxyURL+attrs.image];
+            DominantColors({url:urls})
+                .then(colors=>{
+                    // Sort from darkest to lightest
+                    const sortedColors = colors.sort((a,b)=>luminance(b)-luminance(a));
+                    // Change styles state variables (css custom properties)
                     const styleVars = {} as React.CSSProperties;
-                    if (colors[0]) styleVars['--primary-color']=ColorString(colors[0]);
-                    if (colors[1]) styleVars['--secondary-color']=ColorString(colors[1]);
+                    if (sortedColors[0]) styleVars['--primary-color']=ColorString(colors[0]);
+                    if (sortedColors[1]) styleVars['--secondary-color']=ColorString(colors[1]);
                     setAttrs({...attrs,styleVars})
-                })
-
+            })
                 // Update state
                 setAttrs(attrs);
             })
-    },[id])
+    },[id]) // Only triggered on props change, not state change
      
 
     ////////////////
     // Render
-    return <div className={styles.detail} style={attrs?.styleVars}>
-        <button type="button" onClick={()=>history.goBack()} className={styles.button}>⬅</button>
-        <span className={styles.name}>{attrs?.name}</span>
-        <span className={styles.id}>#{attrs?.id}</span>
-        <section className={styles.typeContainer}>
-            {attrs?.types.map((type,i)=>(
-                <PokeType type={type} key={i}/>
-            ))}
-        </section>
-        <figure className={styles.figure}><img src={attrs?.image} alt={attrs?.name} ></img></figure>
-        <section className={styles.stats}>
-            {attrs&&Object.entries(attrs.stats).map(([k,v],i)=>
-                    <div key={i}>
-                        <div className="label">{k}</div>
+    return (
+    <div className={styles.detail} style={attrs?.styleVars}>
+        <header>
+            <span className={styles.name}>{attrs?.name}</span>
+            <span className={styles.id}>#{attrs?.id}</span>
+        </header>
+        <main>
+            <figure className={styles.figure}><img src={attrs?.image} alt={attrs?.name} ></img></figure>
+            <section className={styles.typeContainer}>
+                {attrs?.types.map((type,i)=>(
+                    <PokeType type={type} key={i}/>
+                ))}
+            </section>
+            <hr/>
+            <section>
+                {attrs&&Object.entries(attrs.stats).map(([k,v],i)=>(
+                    <React.Fragment key={i}>
+                        <div className="label" >{k}</div>
                         <Meter val={v} min={0} max={255}/>
+                    </React.Fragment>
+                ))}
+            </section>
+            <section>
+                <div className={styles.genus}>{attrs?.genus}</div>
+                <div className={styles.description}>{attrs?.description}</div>
+            </section>
+            <section className={styles.profile}>
+                <header>Profile</header>
+                {[
+                    // Modify the properties for presentation (adding units etc.)
+                    // Also adding keys here so that we don't need to repeat HTML
+                    // aspects (className etc.)
+                    ["Height",attrs?.height + " m"],
+                    ["Weight", attrs?.weight+" kg"],
+                    ["Egg Groups", attrs?.egg_groups.map(CapitalFirst).join(", ")],
+                    ["Abilities", attrs?.abilities.map(CapitalFirst).join(", ")]
+                ].map(([label,value])=>(
+                    <div key={label}>
+                        <div className={styles.label}>{label}</div>
+                        <div className={styles.value}>{value}</div>
                     </div>
-                    )}
-        </section>
-        <section>
-            <div className={styles.genus}>{attrs?.genus}</div>
-            <div className={styles.description}>{attrs?.description}</div>
-        </section>
-        <section className={styles.profile}>
-            <header>Profile</header>
-            {[
-                ["Height",attrs?.height + " m"],
-                ["Weight", attrs?.weight+" kg"],
-                ["Egg Groups", attrs?.egg_groups.map(CapitalFirst).join(", ")],
-                ["Abilities", attrs?.abilities.map(CapitalFirst).join(", ")]
-            ].map(([label,value])=>(
-                <div key={label}>
-                    <div className={styles.label}>{label}</div>
-                    <div className={styles.value}>{value}</div>
-                </div>
-            ))}
-        </section>
-    </div>
+                ))}
+            </section>
+        </main>
+    </div>)
 }
 export default Detail
--- src/utils/color.ts
+++ src/utils/color.ts
@@ -6,11 +6,18 @@ export interface RGB{
     b:number
 }
 export type RGBA = (RGB & {a:number})
-const luminence = (c:RGB)=>{
+
+/**
+ * Simple and fast way to calculate rough luminence value
+ */
+export const luminance = (c:RGB)=>{
     // From https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
     return (c.r+c.r+c.r+c.b+c.g+c.g+c.g+c.g)>>3
 }
 
+/**
+ * Converts color object to "rgb(255,234,23)" type string
+ */
 export const ColorString = (c:RGBA|RGB)=>{
     const rgbStr = [c.r,c.g,c.b].join(",");
     if ('a' in c){
@@ -19,45 +26,68 @@ export const ColorString = (c:RGBA|RGB)=>{
     return `rgb(${rgbStr})`;
 }
 
-export const DominantColors = async (src:string)=>{
-    // Setting these as optional parameters interfered with debugging so I'll just set them here
-    const numSamples = 100;
-    const numColors = 2
-    // Load the image
+/**
+ * This will try to extract the colors from an image url. This is only possible
+ * with CORS enabled. If the url doesn't work (i.e., because of CORS) it will try
+ * the fallback URL
+*/
+type DominantColorsParams={
+    url:string|string[],
+    numSamples?:number,
+    numColors?:number
+}
+export const DominantColors = async ({url,numSamples=100,numColors=2}:DominantColorsParams)=>{
+    // Because we can take a single url OR an array of urls, we need cause them to all be an array
+    const urls = typeof url === "string"? [url]:url;
+    
+    /////////////////////////
+    // Load the image section
+    /////////////////////////
     const image = new Image();
 
-    const crossOrigin = !sameOrigin(src);
+    /**
+     * This will set crossOrigin if needed then set src
+     */
+    const assignImgSrc=(src:string)=>{
+        // To look at pixel data the image needs to either be CORS or same origin
+        const crossOrigin = !sameOrigin(urls[0]);
+        if (crossOrigin) image.crossOrigin = 'anonymous';
+        image.src = src;
+    }    
 
-    if (crossOrigin) image.crossOrigin = 'anonymous';
-    
+    // Wait for the image to load correctly or error out
     await new Promise((res,rej)=>{
-        image.onload = ()=>{
-            res(true)
-        }
+        // When it works, yay
+        image.onload = ()=>res(true)
+        // When it doesn't work, try another url in the chain
         image.onerror = ()=>{
-            const triedPoxy = image.src!==src
-            if (crossOrigin && !triedPoxy) {
-                // Hack to get around CORS https://lokeshdhakar.com/projects/color-thief/
-                const googleProxyURL = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=';
-                image.src = googleProxyURL+encodeURIComponent(src);
+            const curIndex = urls.findIndex(url=>url===image.src);
+            const nextURL = urls[curIndex+1];
+            if (curIndex >=0 && nextURL) {
+                assignImgSrc(nextURL)
             }else{
                 rej("Cannot retreive image")
             }
         }
-        image.src = src;
+        assignImgSrc(urls[0]);
     })
 
-    // Draw the image onto a canvas
+    /////////////////////////
+    // Canvas section
+    /////////////////////////
+
+    // Create a canvas to receive the image
     const canvas = document.createElement("canvas");
     canvas.width = image.width;
     canvas.height = image.height;
     const ctx = canvas.getContext('2d');
+    // Draw the image
     ctx?.drawImage(image, 0, 0, canvas.width, canvas.height );
 
-    // Now get the image data
+    // Now start reading the pixel data
     const imageData = ctx?.getImageData(0,0,canvas.width,canvas.height);
     if (imageData){
-        // the imageData is a stream of RGBA data, we need to group them
+        // The imageData is a stream of RGBA data, we need to group them
         // by individual pixels
         const numPixels = imageData.data.length/4;
         const pixels = Array(numPixels).fill(0).map((_,i)=>(
@@ -73,48 +103,42 @@ export const DominantColors = async (src:string)=>{
         // Not exactly necessary to do it this way, but it's FUNctional
         // albeit a bit hard to follow at some spots
         return pixels
-            // Filter out pixels we don't like
+            // Step 1: Filter out pixels we don't want to include
             .filter(p=>{
-                const transparent = p.a<.1;
-                return !transparent;
+                const isTransparent = p.a<.1;
+                return !isTransparent;
             })
             .filter(p=>{
-                const grayscale = p.r===p.g && p.g===p.b;
-                return !grayscale;
+                const isGrayscale = p.r===p.g && p.g===p.b;
+                return !isGrayscale;
             })
             .filter(p=>{
-                const tooLight = luminence(p) > 300;
+                const tooLight = luminance(p) > 300;
                 return !tooLight
             })
             // Only use every n pixel
             .filter((p,i)=>{
                 return i%(Math.floor(numPixels/numSamples))===0
             })
-            // This stores the count map within an array, just so we
-            // can keep the fun js chaining going 😀
-            .reduce(([pixelCounts],cur)=>{
-                const key = (()=>{
-                    for(const key of pixelCounts.keys()){
-                        const colorMatch = Object.entries(key).every(([channel,value])=>{
-                            return value===cur[channel as keyof RGBA]
-                        });
-                        if (colorMatch)return key;
-                    }
-                    return cur;
-                })();
-                const count = pixelCounts.get(key)||0+1;
-                pixelCounts.set(key,count);
+            // This goes through and bins the pixels, forming a count of each
+            // RGB value used. In order to keep the JS chain going, the pixel
+            // counts map is wrapped in a 1 element array.
+            .reduce(([pixelCounts],curPixel)=>{
+                // Generates a string fom the pixel values
+                const keyString = Object.values(curPixel).join("_");
+                // Get current count and add 1
+                const count = (pixelCounts.get(keyString)?.count||0)+1;
+                // Set the key to the new value
+                pixelCounts.set(keyString,{color:curPixel,count});
                 return [pixelCounts]
-            },[new Map()]as Map<RGBA,number>[])
-            .flatMap(pixelCounts=>{
-                return Array.from(pixelCounts.entries());
-            })
+            },[new Map()]as Map<string,{color:RGBA,count:number}>[])
+            // Turn [Map<string,{RGB,count}>] to [{RGB,count}]. Flatmap is
+            // necessary to remove the wrapper on Map
+            .flatMap(pixelCounts=> Array.from(pixelCounts.values()))
             // Only get top n values
-            .sort((a,b)=>a[1]-b[1])
-            .map(([k])=>k)
+            .sort((a,b)=>a.count-b.count)
+            .map(({color,count})=>color)
             .filter((_,i)=>i<numColors)
-            // Have the brighter ones be first
-            .sort((a,b)=>luminence(b)-luminence(a))
     }else{
         throw new Error("Could not get image data")
     }
