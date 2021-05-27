2fef2c3e5fea932511ccc64611708b288cba07b0
Scott <scottdrichards@hotmail.com>
2021-05-27 10:33:51 -0400

    fixed sass conversion errors

--- src/components/pokeType/PokeType.tsx
+++ src/components/pokeType/PokeType.tsx
@@ -1,4 +1,4 @@
 import styles from './PokeType.module.sass'
 export const PokeType=({type}:{type:string})=>{
-    return <div className={`${styles.type} ${styles[type]}`}>{type}</div>
+    return <div className={styles.type} style={{'--type-color':`var(--${type})`}}>{type}</div>
 }
---
+++ src/components/pokeType/css.d.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as CSS from 'csstype';
+
+declare module 'csstype'{
+    interface Properties{
+        '--type-color'?: string;
+    }
+}
